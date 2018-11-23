84114f3029549c861694544b7369471dab6469ce
NONATO SANTOS <nonatosantos1986@gmail.com>
2018-11-23 20:55:25 -0300

    Melhorando identação

--- src/main/java/com/nonatosantos/design/model/DescontoQuantidade.java
+++ src/main/java/com/nonatosantos/design/model/DescontoQuantidade.java
@@ -1,27 +1,25 @@
 package com.nonatosantos.design.model;
 
 public class DescontoQuantidade implements Desconto {
-	
+
 	private Desconto proximo;
 
 	@Override
 	public double desconta(Orcamento orcamento) {
-		
-		if(orcamento.getItens().size()>5) {
-			return orcamento.getValor()*0.10;
-			
-		}
-		else {
+
+		if (orcamento.getItens().size() > 5) {
+			return orcamento.getValor() * 0.10;
+
+		} else {
 			return proximo.desconta(orcamento);
 		}
-		
-		
+
 	}
 
 	@Override
 	public void setProximoDesconto(Desconto proximo) {
 		this.proximo = proximo;
-		
+
 	}
 
 }
